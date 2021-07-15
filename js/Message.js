707d83743b204f1d1401bedfb86c66eeab44ef98
Roberto Loja <robertoloja@gmail.com>
2021-07-15 01:43:09 -0400

    Add fonts

--- js/Message.js
+++ js/Message.js
@@ -1,35 +1,38 @@
 class Message {
-	tagName = this.__proto__.constructor.name
-	author
-
 	constructor(element) {
 		this.element = element
 		this.author = element.attributes.author.value
 		this.text = element.innerHTML
 		this.messageArea = element.parentElement
-		this.text.replace(`${this.text[4]}`, "<br><br>")
 	}
 }
 
 
 class EmailMessage extends Message {
-	subject
-	date
-
 	constructor(element) {
 		super(element)
 		this.subject = element.attributes.subject.value
-		this.date = element.attributes.date.value
+		this.date = this.getDateTimeString(element.attributes.date.value)
 		this.createNewMessage()
 	}
 
+	getDateTimeString(date) {
+		// returns the date attribute in format "Sun May 10 2020 09:03"
+		const zeroPad = (unit) => (unit.length == 1 ? '0' + unit : unit)
+
+		const getTimeString = (date) => zeroPad(date.getHours().toString()) + ':' + 
+										zeroPad(date.getMinutes().toString())
+
+		let dateObj = new Date(Date.parse(date))
+		return dateObj.toDateString() + ' ' + getTimeString(dateObj)
+	}
+
 	createNewMessage() {
-		this.element.innerHTML = `
-			<b>Subject: </b> ${this.subject} <br>
-			<b>From: </b> ${this.author} <br>
-			<b>Date: </b> ${this.date} <br><br>
-			${this.text.replace('\n', '<br><br>')}
-		`
+		this.element.innerHTML = 
+		   `<b>Subject:</b> ${this.subject} <br>
+			<b>From:</b> ${this.author} <br>
+			<b>Date:</b> ${this.date} <br><br>
+			${this.text}`
 	}
 }
 
@@ -39,6 +42,7 @@ class IrcMessage extends Message {
 		super(element)
 		this.createNewMessage()
 	}
+
 	createNewMessage() {
 		let styleClasses = "message"
 		let displayText = ""
@@ -50,26 +54,34 @@ class IrcMessage extends Message {
 			displayText = `&lt;${this.author}&gt; ${this.text}`
 		}
 
+		this.element.className = styleClasses
+
 		this.element.innerHTML =
-			`<div class="${styleClasses}">
-				<p class="message-text">
+		   `<p class="message-text">
 				${displayText}
-				</p>
-			</div>`
+			</p>`
 	}
 }
 
 
 class WhatsAppMessage extends Message {
-	constructor (message) {
-		super(message)
-		this.amAuthor = message.amAuthor
-		this.time = message.time
-		this.hasBeenRead = message.hasBeenRead
+	constructor (element) {
+		super(element)
+		this.amAuthor = (element.attributes.amAuthor.value == 'true')
+		this.time = element.attributes.time.value
+		this.hasBeenRead = (element.attributes.hasBeenRead.value == 'true')
 		this.createNewMessage()
 	}
 
 	createNewMessage() {
+		let children = [...this.element.children]
+
+		this.element.className = `msg${(this.amAuthor ? ' sent' : ' received')}`
+		this.element.innerHTML = 
+		   `${children.map((p) => `<p>${p.innerText} <sub>${this.time} ✓</sub></p>`)
+		   			  .join('')}`
+
+		/*
 		let messageContainer = document.createElement('div')
 		messageContainer.classList.add('msg')
 		messageContainer.classList.add((this.amAuthor ? 'sent' : 'received'))
@@ -85,27 +97,26 @@ class WhatsAppMessage extends Message {
 			messageContainer.appendChild(p)
 		}
 		this.messageArea.filter((x) => x.id == this.author)[0].appendChild(messageContainer)
+		*/
 	}
 }
 
-const registeredClasses = {
-	"IrcMessage": IrcMessage,
-	"WhatsAppMessage": WhatsAppMessage,
-	"EmailMessage": EmailMessage
-}
 
 function onLoad() {
 	/*****
-	 *	Body tag's onload function. Finds all non-standard HTML tags,
-	 *  instantiates the associated class, and attaches the instance
-	 *  to element.classInstance
+	 *	Body tag's onload function. Finds HTML tags that match the above classes,
+	 *  instantiates the associated class, and attaches the instance to 
+	 *  element.classInstance
 	 ****/
-	let allElements = document.body.getElementsByTagName("*")
-	const isElementUnknown = (element) => element.__proto__.constructor.name == "HTMLUnknownElement"
-	let customElements = [...allElements].filter(isElementUnknown)
+	const registeredClasses = {
+		"IrcMessage": IrcMessage,
+		"WhatsAppMessage": WhatsAppMessage,
+		"EmailMessage": EmailMessage
+	}
 
-	customElements.map((element) => {
-		let className = Object.keys(registeredClasses).filter((x) => x.toLowerCase() == element.tagName.toLowerCase())[0]
-		element.classInstance = new registeredClasses[className](element)
+	Object.keys(registeredClasses).map((className) => {
+		[...document.body.getElementsByTagName(className)].map((element) => {
+			element.classInstance = new registeredClasses[className](element)
+		})
 	})
 }
