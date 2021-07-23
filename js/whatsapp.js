80b42583c4d9fd85b91d84bff2e6cd2046bf2b17
Roberto Loja <robertoloja@gmail.com>
2021-07-23 02:42:03 -0400

    Whatsapp responsive

---
+++ js/whatsapp.js
@@ -0,0 +1,17 @@
+function showContacts() {
+	const contacts = document.querySelector('.contacts')
+	const msgNav = document.querySelector('.msgNav')
+
+	if ([...contacts.classList].indexOf('hidden') == -1) {
+		contacts.classList.add('hidden')
+		msgNav.classList.remove('hidden')
+	} else {
+		contacts.classList.remove('hidden')
+		msgNav.classList.add('hidden')
+	}
+}
+
+function showMessages() {
+	showContacts()
+	console.log()
+}
